87218274fad8ab2821bf987e4163e1803c1697ff
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-12-10 14:48:52 -0500

    UI for login view LoginVC.

---
+++ slackish/slackish/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  slackish
+//
+//  Created by Kilyan SOCKALINGUM on 10/12/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ slackish/slackish/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  slackish
+//
+//  Created by Kilyan SOCKALINGUM on 10/12/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import Foundation
+
+//segues
+let TO_LOGIN = "toLogin"
