25937d966d9e745c713a49c8b2e56cfdc223185e
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-12-11 17:01:57 -0500

    Call AuthService registerUser, add cornerRadius for login button and create account button.

--- slackish/slackish/Controller/CreateAccountVC.swift
+++ slackish/slackish/Controller/CreateAccountVC.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,4 +26,20 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
-}
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user !")
+            }
+        }
+    }
+ }
---
+++ slackish/slackish/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  slackish
+//
+//  Created by Kilyan SOCKALINGUM on 11/12/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
