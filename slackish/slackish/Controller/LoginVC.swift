f68ecfe0d3f00ab6d482cb921af6a414ab45e950
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-12-20 17:25:39 -0500

    Login api and check if user is logged when app launch.

--- slackish/slackish/Controller/LoginVC.swift
+++ slackish/slackish/Controller/LoginVC.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    //outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         // Do any additional setup after loading the view.
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: PURPLE_PLACEHOLDER])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: PURPLE_PLACEHOLDER])
     }
     
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameTxt.text, usernameTxt.text != "" else { return }
+        guard let pwd = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: pwd) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_CHANGED, object: nil)
+                        self.spinner.isHidden = false
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func createAccountBtnPressed(_ sender: Any) {
--- slackish/slackish/Utilities/Constants.swift
+++ slackish/slackish/Utilities/Constants.swift
@@ -15,9 +15,14 @@ let BASE_URL = "https://slackishks.herokuapp.com/v1"
 let URL_REGISTER = "\(BASE_URL)/account/register"
 let URL_LOGIN = "\(BASE_URL)/account/login"
 let URL_USER_ADD = "\(BASE_URL)/user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)/user/byEmail"
 
 //Header
 let HEADER = ["Content-Type": "application/json; charset=utf-8"]
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
+]
 
 //Segues
 let TO_LOGIN = "toLogin"
