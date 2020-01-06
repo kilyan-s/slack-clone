5bd0b7a7833b5e00529af655eebe641deec77e9e
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-06 11:51:25 -0500

    Display messages date.

--- slackish/slackish/View/MessageCell.swift
+++ slackish/slackish/View/MessageCell.swift
@@ -26,6 +26,24 @@ class MessageCell: UITableViewCell {
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         
+        //Convert message timestamp to readable date
+        guard var isoDate = message.timestamp else { return }
+        let isoDateEnd = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<isoDateEnd])
+        isoDate.append("Z")
+        print(isoDate)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let messageDate = isoFormatter.date(from: isoDate)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = messageDate {
+            let finaleDate = dateFormatter.string(from: finalDate)
+            timestampLbl.text = finaleDate
+        }
+        
         
     }
 }
